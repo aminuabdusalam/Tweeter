74d771e025034aed9c02a2fe7888903aa3b3f675
Aminu Abdusalam <aminuabdusalam2019@gmail.com>
2020-10-07 21:14:03 -0500

    Implemented infinite scroll/ infinite pagination

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -28,6 +28,7 @@ public class TimelineActivity extends AppCompatActivity {
     List<Tweet> tweets;
     TweetsAdapter adapter;
     SwipeRefreshLayout swipeContainer;
+    EndlessRecyclerViewScrollListener scrollListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,11 +57,50 @@ public class TimelineActivity extends AppCompatActivity {
         //Init the list of tweets and adapater
         tweets = new ArrayList<>();
         adapter = new TweetsAdapter(this, tweets);
-
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
         //Recycler view setup:layout manager and adapter
-        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setLayoutManager(linearLayoutManager);
         rvTweets.setAdapter(adapter);
+
+        scrollListener = new EndlessRecyclerViewScrollListener(linearLayoutManager) {
+            @Override
+            public void onLoadMore(int page, int totalItemsCount, RecyclerView view) {
+                Log.i(TAG,"onLoadMore" + page);
+                loadMoreData();
+            }
+        };
+
+        //Adds scroll listener to RecyclerView
+        rvTweets.addOnScrollListener(scrollListener);
+
+
         populateHomeTimeline();
+    }
+
+    private void loadMoreData() {
+        // 1. Send an API request to retrieve appropriate paginated data
+        client.getNextPageOfTweets(new JsonHttpResponseHandler() {
+                                    @Override
+                                    public void onSuccess(int statusCode, Headers headers, JSON json) {
+                                        Log.i(TAG,"onSuccess for loadMoreData!" + json.toString());
+                                        // 2. Deserialize and construct new model objects from the API response
+                                        JSONArray jsonArray = json.jsonArray;
+                                        try {
+                                            List<Tweet> tweets = Tweet.fromJsonArray(jsonArray);
+                                            // 3. Append the new data objects to the existing set of items inside the array of items
+                                            // 4. Notify the adapter of the new items made with `notifyItemRangeInserted()`
+                                            adapter.addAll(tweets);
+                                        } catch (JSONException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+
+                                    @Override
+                                    public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                                        Log.e(TAG,"onFailure for loadMoreData!",throwable);
+
+                                    }
+                                },tweets.get(tweets.size() - 1).id);
     }
 
     private void populateHomeTimeline() {
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -12,11 +12,13 @@ public class Tweet {
     public String body;
     public String createdAt;
     public User user;
+    public long id;
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
         tweet.body = jsonObject.getString("text");
         tweet.createdAt = jsonObject.getString("created_at");
+        tweet.id = jsonObject.getLong("id");
         tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
         return tweet;
     }
