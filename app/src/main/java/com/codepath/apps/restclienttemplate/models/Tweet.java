26cd9a8775a2a607c3206dc16de219bde7fbeeb4
Aminu Abdusalam <aminuabdusalam2019@gmail.com>
2020-10-16 22:35:08 -0500

    New Activity and XML files created so User can compose new tweet. menu_main.xml was also added to the menu resource folder.

--- app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -3,16 +3,24 @@ package com.codepath.apps.restclienttemplate.models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+
+@Parcel
 public class Tweet {
 
     public String body;
     public String createdAt;
     public User user;
     public long id;
+
+    //empty constructor needeed for Parcel library.
+    public Tweet(){
+
+    }
 
     public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
         Tweet tweet = new Tweet();
--- app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/User.java
@@ -2,14 +2,23 @@ package com.codepath.apps.restclienttemplate.models;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.List;
 
+@Parcel
 public class User {
 
     public String name;
     public String screenName; //aka handle
     public String profileImageUrl;
+
+
+    //empty constructor for Parcel library.
+    public User(){
+    }
+
+
 
     public static User fromJson(JSONObject jsonObject) throws JSONException {
         User user = new User();
